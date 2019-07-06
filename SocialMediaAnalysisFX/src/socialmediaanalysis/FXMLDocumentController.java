44343e7993374a75eae8effe342f85fc27791ec6
ahmad-ra <36510453+ahmad-ra@users.noreply.github.com>
2019-07-06 11:29:02 +0200

    Update FXMLDocumentController.java
    Gui JavaFx

--- SocialMediaAnalysisFX/src/socialmediaanalysis/FXMLDocumentController.java
+++ SocialMediaAnalysisFX/src/socialmediaanalysis/FXMLDocumentController.java
@@ -42,6 +42,47 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void handleButtonAction(ActionEvent event) throws FileNotFoundException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Source File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text file", "*.txt")
+        );
+        fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.dir"))
+        );
+        File file = fileChooser.showOpenDialog(button.getScene().getWindow());
+        if (file != null) {
+            Scanner scanner = new Scanner(file);
+            int no_nodes = 0;
+            int no_vertics = 0;
+
+            if (scanner.hasNextLine()) {
+                Scanner line = new Scanner(scanner.nextLine());
+
+                no_nodes = line.nextInt();
+                no_vertics = line.nextInt();
+            }
+
+            graph = new Graph_Imp(no_nodes);
+
+            for (int k = 0; k < no_vertics; k++) {
+                Scanner line = new Scanner(scanner.nextLine());
+
+                int src = line.nextInt();
+                int dst = line.nextInt();
+                double wt = line.nextDouble();
+                Edge_Imp edg;
+
+                edg = new Edge_Imp(graph.getNode(src), wt);
+                graph.getNode(dst).addChild(edg);
+                edg = new Edge_Imp(graph.getNode(dst), wt);
+                graph.getNode(src).addChild(edg);
+
+            }
+            scanner.close();
+        }
+
+        init_draw(graph);
 
     }
 
@@ -52,7 +93,64 @@ public class FXMLDocumentController implements Initializable {
     }
 
     public void init_draw(Graph_Imp G) {
+                int spacing = 60;
+        float thetaSector = (float) (2.0 * Math.PI) / G.getNoVertices();
 
+        float radius = (float) ((spacing + 100.0) / (2.0 * Math.sin(thetaSector / 2)));
+
+        graphics.setPrefWidth((2 * radius) + radius);
+        graphics.setPrefHeight((2 * radius) + radius);
+
+        float windowXcenter = radius;
+        float windowYcenter = radius;
+
+        float X = windowXcenter + (float) (radius);
+        float Y = windowYcenter;
+
+        float theta = 0;
+        for (Node_Imp N : G.getVertices()) {
+
+            N.x = X;
+            N.y = Y;
+
+            theta += thetaSector;
+            //newX = radius * Math.cos(theta) ;
+            //newY = radius * Math.sin(theta) ;
+
+            X = (float) (windowXcenter + radius * Math.cos(theta));
+            Y = (float) (windowYcenter + radius * Math.sin(theta));
+            //X += radius * Math.cos(theta) ;
+            //Y += radius * Math.sin(theta) ;
+
+        }
+
+        float maxCentrality = -1;
+        for (Node_Imp N : G.getVertices()) {
+
+            if (N.getCentrality() > maxCentrality) {
+                maxCentrality = (float) N.getCentrality();
+            }
+        }
+
+        maxCentrality = Math.abs(maxCentrality);
+        for (Node_Imp N : G.getVertices()) {
+
+            Rectangle rectangle = new Rectangle(N.x, N.y, 100 / maxCentrality, 100 / maxCentrality);
+
+            graphics.getChildren().add(rectangle);
+
+        }
+
+        for (Node_Imp N : G.getVertices()) {
+
+            for (Edge_Imp E : N.getChildren()) {
+
+                Line l = new Line(N.x, N.y, E.getChild().x, E.getChild().y);
+
+                graphics.getChildren().add(l);
+            }
+
+        }
     }
 
 }
